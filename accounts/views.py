6668675efdc18e23b889be82a55c2ec5f9f4d28b
simonovmaksim <simonov.maksim13@yandex.ru>
2021-10-04 13:30:04 +0300

    updated MyCompanyView and commented old version

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,10 @@ from django.contrib.auth.views import LoginView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.db.models import Count
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, TemplateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
 from hiring.models import Company, Vacancy
 
 from accounts.forms import CompanyForm, VacancyForm
@@ -18,9 +21,9 @@ class MyLoginView(LoginView):
 
 
 class MySignupView(CreateView):
+    template_name = 'accounts/register.html'
     form_class = UserCreationForm
     success_url = '/login'
-    template_name = 'accounts/register.html'
 
 
 class SentView(TemplateView):
@@ -53,31 +56,38 @@ class CreateMyCompanyView(LoginRequiredMixin, SuccessMessageMixin, TemplateView)
         return render(request, 'accounts/company-edit.html', context={'form': form})
 
 
-class MyCompanyView(LoginRequiredMixin, CompanyAccessMixin, TemplateView):
+class MyCompanyView(LoginRequiredMixin, CompanyAccessMixin, SuccessMessageMixin, UpdateView):
     template_name = 'accounts/company-edit.html'
+    model = Company
+    form_class = CompanyForm
+    success_url = reverse_lazy('my_company')
+    success_message = 'Информация о компании обновлена'
+    context_object_name = 'company_edit'
 
-    def get_context_data(self, **kwargs):
-        company = Company.objects.get(owner_id=self.request.user.id)
-        form_context = {
-            'name': company.name,
-            'employee_count': company.employee_count,
-            'location': company.location,
-            'description': company.description,
-            'logo': company.logo.url,
-        }
-        form = CompanyForm(form_context)
-        context = super(MyCompanyView, self).get_context_data(**kwargs)
-        context['company_edit'] = company
-        context['form'] = form
-        return context
+    def get_object(self):
+        return self.request.user.company
 
-    def post(self, request, *args, **kwargs):
-        form = CompanyForm(request.POST)
-        if form.is_valid():
-            create_company = form.save(commit=False)
-            form.instance.owner_id = request.user.id
-            create_company.save()
-        return render(request, 'accounts/company-edit.html', context={'form': form})
+
+# class MyCompanyView(LoginRequiredMixin, CompanyAccessMixin, SuccessMessageMixin, TemplateView):
+#     template_name = 'accounts/company-edit.html'
+#     success_message = 'Информация о компании обновлена'
+
+#     def get_context_data(self, **kwargs):
+#         company = Company.objects.get(owner_id=self.request.user.id)
+#         form = CompanyForm(instance=company)
+#         context = super(MyCompanyView, self).get_context_data(**kwargs)
+#         context['company_edit'] = company
+#         context['form'] = form
+#         return context
+
+#     def post(self, request, *args, **kwargs):
+#         instance = Company.objects.get(owner_id=self.request.user.id)
+#         form = CompanyForm(request.POST, request.FILES, instance=instance)
+#         if form.is_valid():
+#             print('valid')
+#             form.save()
+#             return redirect('my_company')
+#         return render(request, 'accounts/company-edit.html', context={'form': form})
 
 
 class MyCompanyVacanciesView(LoginRequiredMixin, CompanyAccessMixin, ListView):
@@ -110,19 +120,20 @@ class CreateVacancyView(LoginRequiredMixin, CompanyAccessMixin, TemplateView):
         return render(request, 'accounts/company-edit.html', context={'form': form})
 
 
-class MyCompanyVacansyView(LoginRequiredMixin, CompanyAccessMixin, TemplateView):
+class MyCompanyVacansyView(LoginRequiredMixin, CompanyAccessMixin, DetailView):
     template_name = 'accounts/vacancy-edit.html'
+    model = Vacancy
     pk_url_kwarg = 'vacancy_id'
 
     def get_context_data(self, **kwargs):
-        vacancy = Vacancy.objects.get(id=self.kwargs['vacancy_id'])
+        #vacancy = Vacancy.objects.get(id=self.kwargs['vacancy_id'])
         form_context = {
-            'title': vacancy.title,
-            'specialty': vacancy.specialty,
-            'skills': vacancy.skills,
-            'description': vacancy.description,
-            'salary_min': vacancy.salary_min,
-            'salary_max': vacancy.salary_max,
+            'title': self.object.title,
+            'specialty': self.object.specialty,
+            'skills': self.object.skills,
+            'description': self.object.description,
+            'salary_min': self.object.salary_min,
+            'salary_max': self.object.salary_max,
         }
         form = VacancyForm(form_context)
         context = super(MyCompanyVacansyView, self).get_context_data(**kwargs)
@@ -130,12 +141,11 @@ class MyCompanyVacansyView(LoginRequiredMixin, CompanyAccessMixin, TemplateView)
         return context
 
     def post(self, request, *args, **kwargs):
-        form = VacancyForm(request.POST)
+        instance = self.get_object(self.queryset)
+        form = VacancyForm(request.POST, instance=instance)
         if form.is_valid():
-            create_vacancy = form.save(commit=False)
-            form.instance.published_as = '%Y-%m-%d'
-            create_vacancy.save()
-            return redirect('my_company_vacancy')
+            form.save()
+            return redirect('my_company_vacancy', self.kwargs["vacancy_id"])
         return render(request, 'accounts/company-edit.html', context={'form': form})
 
 # Create your views here.
